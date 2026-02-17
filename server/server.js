a114dee799ba0dfee3dbdbf3470a6c174ef34986
adhira <adhiradeogade@gmail.com>
2026-02-16 21:19:51 -0800

    Add some comments

--- server/server.js
+++ server/server.js
@@ -19,6 +19,11 @@ io.on('connection', socket => {
     console.log(`User ${socket.id} connected`)
     socket.on('message', data => {
         console.log(`Got a message ${data}`);
+        // I would use broadcast if I want to send data
+        // to all other clients except source client
+        // To send message to all the connected clients,
+        // I will use io.emit()
+        // socket.broadcast.emit('message', `${socket.id.substring(0,5)}: ${data}`)
         io.emit('message', `${socket.id.substring(0,5)}: ${data}`)
         console.log('I want to broadcast');
     })
