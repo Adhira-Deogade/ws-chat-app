ee9d2dcfd40469b0299a22e7e80e7bc74492b06e
adhira <adhiradeogade@gmail.com>
2026-02-16 21:16:42 -0800

    Install vs code extension - live server

--- server/server.js
+++ server/server.js
@@ -2,9 +2,6 @@ import { createServer } from "http"
 import { Server } from "socket.io"
 
 const httpServer = createServer()
-// cors: {
-//         origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500", "http://127.0.0.1:5500"]
-//     }
 const io = new Server(httpServer, {
     cors: {
         origin: ["http://localhost:5500", "http://127.0.0.1:5500"],
